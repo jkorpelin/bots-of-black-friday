134d538b03db226ad418c30e2caab605cec480cc
joni.korpelin <joni.korpelin@solita.fi>
2023-02-10 15:54:48 +0200

    commit

--- templates/clojure/src/clojure_bot/core.clj
+++ templates/clojure/src/clojure_bot/core.clj
@@ -2,11 +2,27 @@
   (:require [clojure-bot.api :as api])
   (:gen-class))
 
+(def game-info (atom {}))
+(def run? (atom true))
+
+(defn start
+  []
+  (reset! run? ))
+
+(defn run
+  []
+  (let [x (api/register "🫥")]
+    (reset! game-info x)
+
+    (while @run?
+      (Thread/sleep 1000)
+      (println "runnin: " (pr-str @run?))
+      ;; You probably want to get the current game-state from the server before you do your move
+      (api/move (:id @game-info) (rand-nth ["LEFT" "RIGHT"])))))
+
+
+
 (defn -main
   [& args]
   ;; game-info contains the game map
-  (let [game-info (api/register "My cool Clojure bot")]
-    (while true
-      (Thread/sleep 1000)
-      ;; You probably want to get the current game-state from the server before you do your move
-      (api/move (:id game-info) (rand-nth ["LEFT" "RIGHT"])))))
+  (run))
